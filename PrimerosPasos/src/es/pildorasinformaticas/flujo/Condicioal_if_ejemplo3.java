ae6952faf691a0089b7e03f662db43cb00df208c
Martin vera <martinalejandrovera@gmail.com>
2022-02-19 15:19:04 -0300

    otro

--- PrimerosPasos/src/es/pildorasinformaticas/flujo/Condicioal_if_ejemplo3.java
+++ PrimerosPasos/src/es/pildorasinformaticas/flujo/Condicioal_if_ejemplo3.java
@@ -16,10 +16,12 @@ public class Condicioal_if_ejemplo3 {
 		 * aca necesito el operador & 
 		 */
 
+		double nn=2864644655555555555555555555555555555555555556546546546561.129651651651651862;
 		int distancia_colegio; //INT PORQUE ES KILOMETROS ENTEROS
 		
 		double renta_familiar; //double porque puede haber centavos
 		
+		byte hermanos_en_el_instituto;
 		distancia_colegio=Integer.parseInt //convierte a enteros un string
 				(JOptionPane.showInputDialog //solamente acepta string
 						("decime tu distancia al colegio en kilometros enteros por favor"));
@@ -28,15 +30,26 @@ public class Condicioal_if_ejemplo3 {
 				(JOptionPane.showInputDialog("cuàl es la renta familiar?")); //solamente toma string
 		
 
-		if (distancia_colegio>10 && renta_familiar<20000) {
+		hermanos_en_el_instituto= Byte.parseByte
+				(JOptionPane.showInputDialog
+						("cuàntos hermanos ya están cursando en el instituto"));
+				
+		if (distancia_colegio>10 
+				|| renta_familiar<20000 
+				&& hermanos_en_el_instituto>1)
+		// || opera como "O" (or)  && opera como "Y" (and)
+		{
 			
 			System.out.println("Tiene beca");
 		} else {
 			
 			System.out.println("no tenés beca, abusivo");
+			
+			
 		}
 		
 		
+	
 		
 		
 		
