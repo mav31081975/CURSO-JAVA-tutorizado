74f0e70e3974a1e43b03d4f400f4f79a4f9fb5b9
Martin vera <martinalejandrovera@gmail.com>
2022-02-21 20:39:39 -0300

    otro

---
+++ PrimerosPasos/src/es/pildorasinformaticas/flujo/Conjetura_2N_mas_uno.java
@@ -0,0 +1,45 @@
+package es.pildorasinformaticas.flujo;
+
+import javax.swing.JOptionPane;
+
+public class Conjetura_2N_mas_uno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// evaluara la conjetura de goldbach 2n + 1 dado un entero de dato ingresado
+		
+		int numero= Integer.parseInt(JOptionPane.showInputDialog("escribi un entero no muuuuy largo, 9 cifras maximo "));
+		
+		/*ahora SI EL NUMERO ES PAR debe dividirlo por dos y volverlo a nombrar igual
+		 * mientras que si es par debe multiplicarse por tres y al resultado sumarle 1
+		 * y volverlo a nombrar e imprimirlo en una nueva lìnea, hasta que llegue al valor 1
+		 */
+		
+		if (numero>1) {
+			
+			System.out.println("alto");
+		}
+		
+		
+		
+		
+	/*	
+	 * 
+	 * while (numero>1) {
+			
+			if numero%2 */
+		
+		
+		else
+			
+		{
+			System.out.println(numero);}
+		
+		
+		
+		
+		
+	}}
+
+
