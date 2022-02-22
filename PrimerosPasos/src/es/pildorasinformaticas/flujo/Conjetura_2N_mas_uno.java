8d395ed5e48bbf842519afa75aac82c85a2c0203
Martin vera <martinalejandrovera@gmail.com>
2022-02-22 12:01:44 -0300

    hasta ahora funciona bien goldbach aun no puse WHILE

--- PrimerosPasos/src/es/pildorasinformaticas/flujo/Conjetura_2N_mas_uno.java
+++ PrimerosPasos/src/es/pildorasinformaticas/flujo/Conjetura_2N_mas_uno.java
@@ -7,19 +7,74 @@ public class Conjetura_2N_mas_uno {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// evaluara la conjetura de goldbach 2n + 1 dado un entero de dato ingresado
+		// evaluara la conjetura de goldbach , llamada "2n + 1" , dado un entero de dato ingresado
 		
-		int numero= Integer.parseInt(JOptionPane.showInputDialog("escribi un entero no muuuuy largo, 9 cifras maximo "));
+		int numero= Integer.parseInt
+				(JOptionPane.showInputDialog
+						("escribi un entero no muuuuy largo, 9 cifras máximo "));
 		
 		/*ahora SI EL NUMERO ES PAR debe dividirlo por dos y volverlo a nombrar igual
 		 * mientras que si es par debe multiplicarse por tres y al resultado sumarle 1
 		 * y volverlo a nombrar e imprimirlo en una nueva lìnea, hasta que llegue al valor 1
+		 * 
+		 * 
+		 * 
 		 */
 		
-		if (numero>1) {
+		
+		// evaluara la conjetura de goldbach , llamada "2n + 1" , dado un entero de dato ingresado
+		
+				
+				/*ahora SI EL NUMERO ES PAR debe dividirlo por dos y volverlo a nombrar igual
+				 * mientras que si es par debe multiplicarse por tres y al resultado sumarle 1
+				 * y volverlo a nombrar e imprimirlo en una nueva lìnea, hasta que llegue al valor 1
+				 * 
+				 * 
+				 * 
+				 */
+				
+				
+				
+		
+		
+		/*if (numero>1) {
 			
 			System.out.println("alto");
+			
+			int resto = numero%2; 
+			
+			System.out.println(resto);
+			 if (resto=0) {System.out.println("el nùmero "+"es par") ;
+			
+			
+			/*si resto es 1 es impar, si resto es cero es par
+			 * con eso entro a que mierda hacer con la variable
+			 */
+			
+			//System.out.println(resto);
+			
+			//if (resto=1) { System.out.println("El número "+ numero + " es impar, por ende lo multiplico por 3 y le sumo uno" );}
+			
+			
+			int mitad= numero/2;
+			
+			System.out.println(mitad);
+			
+			if (mitad*2==numero) {System.out.println("el número es par");
+			numero= numero/2;
+			System.out.println("la mitad es " + numero);}
+			
+			else {System.out.println("el numero es impar");
+			System.out.println("multiplico por 3 y le sumo 1 y queda "+ (numero*3+1));
+			}
+			
+					
+			
+			
+			
 		}
+	
+}
 		
 		
 		
@@ -31,15 +86,5 @@ public class Conjetura_2N_mas_uno {
 			if numero%2 */
 		
 		
-		else
-			
-		{
-			System.out.println(numero);}
-		
-		
-		
-		
-		
-	}}
 
 
