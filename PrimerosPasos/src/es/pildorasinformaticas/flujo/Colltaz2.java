ed9cccb5cd850f3fb5b2bbdc6961c95004e4ff94
Martin vera <martinalejandrovera@gmail.com>
2022-02-23 01:53:48 -0300

    ahora parece que si funciona bien

--- PrimerosPasos/src/es/pildorasinformaticas/flujo/Colltaz2.java
+++ PrimerosPasos/src/es/pildorasinformaticas/flujo/Colltaz2.java
@@ -27,21 +27,24 @@ public class Colltaz2 {
 								
 				while (numero!=1) {
 				
-					int mitad= numero/2;
+					double mitad= numero/2;
 					
 					if (mitad*2==numero) {System.out.println("el número es par, por ende lo divido por 2");
 					numero= numero/2;
 					System.out.println("la mitad es " + numero);
 					iteraciones++ ;
+					//numero_inicial++;
 					}
 					
 					else {System.out.println("el numero es impar");
 					System.out.println("multiplico por 3 y le sumo 1 y queda "+ (numero*3+1));
 					numero=numero*3+1;
 					iteraciones++;
-					}
-					numero_inicial++;
+					//numero_inicial++;
+					} 
+					
 								}
+				numero_inicial++;
 				//System.out.println("se necesitaron "+ iteraciones+ " iteraciones para llegar al número 1");
 				//System.out.println("Por ende se verifica la Conjetura de Collatz para el número "+ numero1);
 				//numero_inicial=numero;
