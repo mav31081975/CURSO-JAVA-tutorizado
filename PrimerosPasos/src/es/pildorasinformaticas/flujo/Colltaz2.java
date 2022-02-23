6a520b46b55746706ffb5becf75cc20d06024829
Martin vera <martinalejandrovera@gmail.com>
2022-02-23 00:22:02 -0300

    jj

--- PrimerosPasos/src/es/pildorasinformaticas/flujo/Colltaz2.java
+++ PrimerosPasos/src/es/pildorasinformaticas/flujo/Colltaz2.java
@@ -6,31 +6,79 @@ public class Colltaz2 {
 
 	public static void main(String[] args) {
 		
-		int numero_final= Integer.parseInt
+		int numero_inicial= Integer.parseInt //desde que numero quiero verificr la conjetura,
 				(JOptionPane.showInputDialog
-						("escribi un entero hasta el que quieras  verificar collatz "));
-		
-		int numero_inicial= Integer.parseInt
+						("escribi un entero desde el que quieras  verificar collatz, NO EL 1 QUEES TRIVIAL "));
+				
+		int numero_final= Integer.parseInt  //hasta què nùmero quiero verificar la conjetura								
 				(JOptionPane.showInputDialog
-						("escribi un entero desde el que quieras  verificar collatz "));
+						("escribi un entero hasta el que quieras  verificar collatz, ojo, tiene que ser mayor o igual al anterior "));
 		
-		while (numero_inicial<numero_final) {
+		final int NUMERO_INICIAL=numero_inicial;
+		
+		final int NUMERO_FINAL=numero_final;
+		
+		while (numero_inicial<numero_final) 
+		
+		{
 			
-			int mitad= numero_inicial/2;
+
+
+				int numero= NUMERO_INICIAL;
+						
+				System.out.println(numero);
+				int numero1=numero;
+				
+				int iteraciones=0;
+								
+				while (numero!=1) {
+				
+					int mitad= numero/2;
+					
+					if (mitad*2==numero) {System.out.println("el número es par, por ende lo divido por 2");
+					numero= numero/2;
+					System.out.println("la mitad es " + numero);
+					iteraciones++;
+					numero_inicial++;}
+					
+					else {System.out.println("el numero es impar");
+					System.out.println("multiplico por 3 y le sumo 1 y queda "+ (numero*3+1));
+					numero=numero*3+1;
+					iteraciones++;
+					numero_inicial++;}
+					
+							
+					
+					
+					
+				}
+				System.out.println("");
+				System.out.println("");
+				System.out.println("");
+				
+				System.out.println("se necesitaron "+ iteraciones+ " iteraciones para llegar al número 1");
+				
+				System.out.println("");
+				System.out.println("");
+				
+				System.out.println("Por ende se verifica la Conjetura de Collatz para el número "+ numero1);
+				
+				
+		}
+
+
+			
+			
+			
+			
+			
 			
 			
 			
 		}
+	
+	
 		
-		
-	
-		
-
-	
-	
-	
-	
-	
-	}
+		//System.out.println("Se probò la Conjetura de Collatz para los " + (NUMERO_FINAL-NUMERO_INICIAL+1)+ " números entre " +NUMERO_INICIAL + " y " + NUMERO_FINAL + ", ambos incluìdos");
 
 }
