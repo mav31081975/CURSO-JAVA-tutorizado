1a54efc16053a4ba701635c945b0ea12ad23744a
Martin vera <martinalejandrovera@gmail.com>
2022-02-25 20:01:22 -0300

    avanzo

---
+++ PrimerosPasos/src/dudas/Ejercicio2Video33.java
@@ -0,0 +1,31 @@
+package dudas;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio2Video33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String email = JOptionPane.showInputDialog("ingrese su email");
+		
+		
+		int largo= email.length();
+		
+		String malo = ("No te hagas el boludo e introdcí un email verdadero");
+		
+		if (largo<4)
+			
+			System.out.println(malo);
+		
+		else
+			
+		
+		//if (email.length()<4):
+		
+		
+		
+		
+	}
+
+}
