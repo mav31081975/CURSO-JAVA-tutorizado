f9b986c76635afe4d8fe5e11717f2d865f2cb498
Martin vera <martinalejandrovera@gmail.com>
2022-03-04 19:05:55 -0300

    DUDA PARA JUAN ARRAY BIDIMENSIONAL

--- PrimerosPasos/src/es/pildoras/arrays/Ejemplo_Arrays.java
+++ PrimerosPasos/src/es/pildoras/arrays/Ejemplo_Arrays.java
@@ -62,10 +62,96 @@ public class Ejemplo_Arrays {
 			//array de strings
 			System.out.println("arrays de strings");
 			
-			String nombresdegente[]= {"jaun,","maria"};
+			String nombresdegente[]= {"jaun","maria","elton","hhh"};
+			
+			for (int i=0;i<nombresdegente.length; i++)	
+			{
+				System.out.println(nombresdegente[i]);
+			}
+			
+			//ahora usar for each, pero no vale variable int, ojo
+			
+			for (String elementos:nombresdegente)
+			{
+				System.out.print(elementos+" "); //es mas comodo FOR EACH
+				
+			}
+			
+			System.out.println(" "); 
+			System.out.println(" "); 
+			System.out.println(" "); 
+			System.out.println(" "); 
+			System.out.println("**************************************** "); 
+			
+			System.out.println(" "); 
+			System.out.println("arrays de dos dimensiones VIDEO 37 "); 
+			// tienen dos indices y son de un solo tipo de datos
+			
+			int [][] numeros=new int [5][3];
+			
+			int [][] [] numeross=new int [5][3][8];
+			
+			numeross [0] [0] [7] =5555;
+			System.out.println(numeross[0][0][7]);
+					
+			
+			numeros [0] [0] =5  ;// rellena asi pero es pesado
+			numeros [1][2] =4  ;
+			numeros [4] [2] =856  ;
+			System.out.println(numeros[0][0]);
+			
+			// se usan bucle for anidado para verlo
+			// el "padre" recorre la primera dimensión
+			//el bucle "hijo" a la segunda
+			
+			for (int i=0;i<5;i++) {
+				for (int z=0;z<3;z++) {
+				System.out.print(numeros[i][z] + " ");	
+				}
+				System.out.println();
+				
+								
+			}
+			
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("****************************VIDEO  38***Relleno simpleficado de arrays");
+			
+			int array38 [][]={{1,2,3,4},{5,6,4,8},{9,10,11,12}};
+			
+			for (int i=0;i<5;i++) {
+				for (int z=0;z<4;z++) {
+						
+						System.out.println(array38);}
+				
+				/*qué pasa a partir de la linea 119 que me imprime un array
+				 * literlmente así  :
+				 [[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+[[I@cac736f
+				 */
+			}
 			
 			
-		
+			
 		
 		
 
